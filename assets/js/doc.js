a71cc50dd276b80010280618a277a2cef24578a0
tarunmalik330 <maliktarun2005@gmail.com>
2023-08-22 09:24:38 +0530

    format-docomat

--- assets/js/doc.js
+++ assets/js/doc.js
@@ -1,6 +1,6 @@
 
-    function openNav() {
-        document.getElementById("navbar").classList.toggle("end-0");
+function openNav() {
+    document.getElementById("navbar").classList.toggle("end-0");
     document.body.classList.toggle("overflow_hidden");
     document.getElementById("menubtn-icon").classList.toggle("cross")
-            }
+}
